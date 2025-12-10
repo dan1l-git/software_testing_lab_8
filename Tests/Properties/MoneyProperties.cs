ac62e93ea2e87887b6545f1a4e186cbd90f2336d
dan1l-git <danilgavriluk756@gmail.com>
2025-12-10 13:17:56 +0200

    Tests of mathematical properties of money added.

--- Tests/Properties/MoneyProperties.cs
+++ Tests/Properties/MoneyProperties.cs
@@ -11,6 +11,41 @@ namespace WalletPropertyTesting.Tests.Properties
 
     public class MoneyProperties
     {
+        [Property(Arbitrary = new [] {typeof(WalletArbitraries)})]
+        public void AdditionIsCommutative(Money a, Money b)
+        {
+            Assert.Equal((a + b).Amount, (b + a).Amount);
+        }
+        
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public void AdditionIsAssociative(Money a, Money b, Money c)
+        {
+            Assert.Equal(((a + b) + c).Amount, (a + (b + c)).Amount);
+        }
+        
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public void IdentityElementAddition(Money a)
+        {
+            var zero = new Money(0);
+            Assert.Equal((a + zero).Amount, a.Amount);
+        }
+        
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public void AdditionAndSubtractionAreInverse(Money a, Money b)
+        {
+            var sum = a + b;
+            var result = sum - b;
+            Assert.Equal(a.Amount, result.Amount);
+        }
+        
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public void SubtractionThrowsAssumingInsufficientFunds(Money a, Money b)
+        {
+            if (b.Amount > a.Amount)
+            {
+                Assert.Throws<InvalidOperationException>(() => a - b);
+            }
+        }
 
     }
 

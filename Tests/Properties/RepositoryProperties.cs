508891a497fc0a2f03452396b13a99fc447cc846
dan1l-git <danilgavriluk756@gmail.com>
2025-12-10 13:46:08 +0200

    Tests for Repository Properties and Sequence Properties implemented.

--- Tests/Properties/RepositoryProperties.cs
+++ Tests/Properties/RepositoryProperties.cs
@@ -11,7 +11,26 @@ namespace WalletPropertyTesting.Tests.Properties
 
     public class RepositoryProperties
     {
-       
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public void SaveAndGetReturnsSameWalletState(Money initialDeposit)
+        {
+            var repo = new InMemoryWalletRepository();
+            var service = new WalletService(repo);
+            
+            var wallet = service.CreateWallet();
+            service.Deposit(wallet.Id, initialDeposit.Amount);
 
+            var retrievedWallet = repo.Get(wallet.Id);
+
+            Assert.Equal(wallet.Id, retrievedWallet.Id);
+            Assert.Equal(wallet.Balance.Amount, retrievedWallet.Balance.Amount);
+        }
+        
+        [Fact] 
+        public void GetNonExistentWalletThrows()
+        {
+            var repo = new InMemoryWalletRepository();
+            Assert.Throws<KeyNotFoundException>(() => repo.Get(Guid.NewGuid()));
+        }
     }
 }
--- Tests/Properties/SequenceProperties.cs
+++ Tests/Properties/SequenceProperties.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FsCheck;
+using FsCheck.Fluent;
 using WalletPropertyTesting.Domain;
 using WalletPropertyTesting.Tests.Arbitraries;
 
@@ -12,7 +14,42 @@ namespace WalletPropertyTesting.Tests.Properties
 
     public class SequenceProperties
     {
+        [Property(Arbitrary = new[] { typeof(WalletArbitraries) })]
+        public Property ValidSequenceResultsInCorrectBalance(List<WalletOperation> operations)
+        {
+            var wallet = new Wallet();
+            decimal expectedBalance = 0;
 
+            foreach (var op in operations)
+            {
+                if (op.Type == WalletOpType.Deposit)
+                {
+                    wallet.Deposit(new Money(op.Amount));
+                    expectedBalance += op.Amount;
+                }
+                else
+                {
+                    if (expectedBalance >= op.Amount)
+                    {
+                        wallet.Withdraw(new Money(op.Amount));
+                        expectedBalance -= op.Amount;
+                    }
+                    else
+                    {
+                        try 
+                        {
+                            wallet.Withdraw(new Money(op.Amount));
+                            return false.ToProperty(); 
+                        }
+                        catch (InvalidOperationException) 
+                        { 
+                        }
+                    }
+                }
+            }
+
+            return (wallet.Balance.Amount >= 0).ToProperty();
+        }
 
     }
 
